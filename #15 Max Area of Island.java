c9f1dcde7b4b7491c4d1bb4a9351a72c939615c9
Yahya Ghanem <yahya.ghanem@nweave.com>
2021-10-07 22:57:42 +0200

    Work done: ========== - Solve Flood fill (Depth First Search) - Solve Max Area (Depth First Search)

---
+++ #15 Max Area of Island.java
@@ -0,0 +1,28 @@
+/*
+You are given an m x n binary matrix grid. An island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+
+The area of an island is the number of cells with a value 1 in the island.
+
+Return the maximum area of an island in grid. If there is no island, return 0.
+
+Input: grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+Output: 6
+Explanation: The answer is not 11, because the island must be connected 4-directionally.
+*/
+    public int maxAreaOfIsland(int[][] grid) {
+        int max = 0;
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[0].length; j++) {
+                max = Math.max(max, dfs(grid, i, j));
+            }
+        }
+        return max;
+    }
+    
+    int dfs(int[][] grid, int row, int column) {
+        if(row < 0 || row >= grid.length || column < 0 || column >= grid[0].length || grid[row][column] == 0) return 0;
+        
+        grid[row][column] = 0;
+        
+        return 1+ dfs(grid, row+1, column) + dfs(grid, row-1, column) + dfs(grid, row, column+1) + dfs(grid, row, column-1);
+    }
